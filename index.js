03a79004ba51eafd5ded029879724af1268fcdef
opu183059 <opuctg440@gmail.com>
2023-05-18 22:37:02 +0600

    insert function added

--- index.js
+++ index.js
@@ -21,13 +21,23 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    console.log("connected to MongoDB!");
+
+    const toyDb = client.db("toyDb").collection("toy");
+
+    app.post("/addToy", async (req, res) => {
+      const body = req.body;
+      // console.log(body);
+      const result = await toyDb.insertOne(body);
+      console.log(result);
+      res.send(result);
+    });
+
+    app.get("/allToy", async (req, res) => {
+      const result = await toyDb.find({}).toArray();
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
